16a8b30b5cb2677763144d30c214b01a79324d94
Robson Quedevez <robson.quedevez@hotmail.com>
2025-02-22 13:50:51 -0300

    adiciona tratamento de erro mais robusto no arquivo `database.js`

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: true,
   });
   try {
     await client.connect();
@@ -14,6 +15,7 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
