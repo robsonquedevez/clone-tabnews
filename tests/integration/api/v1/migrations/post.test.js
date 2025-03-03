c6e56b43b8a8a51982ac861efe6c266332d995c2
Robson Quedevez <robson.quedevez@hotmail.com>
2025-03-03 12:53:31 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,31 +6,62 @@ beforeAll(async () => {
   await database.query("DROP schema public cascade; CREATE schema public");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        const responseBody = await response.json();
+
+        expect(response.status).toBe(201);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        const responseBody = await response.json();
+
+        expect(response.status).toBe(200);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
+});
 
-  const response1Body = await response1.json();
+describe("[PUT | PATCH | DELETE] /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "PUT",
+      });
 
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
+      expect(response.status).toBe(405);
 
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+      const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "PATCH",
+      });
+
+      expect(response2.status).toBe(405);
+
+      const response3 = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
+
+      expect(response3.status).toBe(405);
+    });
   });
-
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
-
-  const response3 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "PUT",
-  });
-
-  expect(response3.status).toBe(405);
 });
