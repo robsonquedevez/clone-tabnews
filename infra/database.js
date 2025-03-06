91b23de2d6f5361a79138dfb1e019aa06e611760
Robson Quedevez <robson.quedevez@hotmail.com>
2025-03-05 22:08:33 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,7 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.error(error);
     throw error;
   } finally {
     await client?.end();
