d459c7b97f55ed68b15cc42ff3b6561ddea0b9e8
Robson Quedevez <robson.quedevez@hotmail.com>
2025-05-28 22:15:33 -0300

    refactor: remove unused modules

---
+++ tests/integration/api/v1/sessions/post.test.js
@@ -0,0 +1,93 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.cleanDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/sessions", () => {
+  describe("Anonymous user", () => {
+    test("With incorretc `email` but corret `passwaord`", async () => {
+      await orchestrator.createUser({
+        password: "PassCorrect123@",
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "incorrect_email@dundermifflin.com",
+          password: "PassCorrect123@",
+        }),
+      });
+
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Dados de autenticação não conferem.",
+        action: "Verifique se os dados enviados estão corretos.",
+        status_code: 401,
+      });
+    });
+
+    test("With correct `email` but incorrect `passwaord`", async () => {
+      await orchestrator.createUser({
+        email: "scot@dundermifflin.com",
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "scot@dundermifflin.com",
+          password: "PassIncorrect123@",
+        }),
+      });
+
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Dados de autenticação não conferem.",
+        action: "Verifique se os dados enviados estão corretos.",
+        status_code: 401,
+      });
+    });
+
+    test("With incorrect `email` and incorrect `passwaord`", async () => {
+      await orchestrator.createUser();
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "incorrect_email@dundermifflin.com",
+          password: "PassIncorrect123@",
+        }),
+      });
+
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Dados de autenticação não conferem.",
+        action: "Verifique se os dados enviados estão corretos.",
+        status_code: 401,
+      });
+    });
+  });
+});
