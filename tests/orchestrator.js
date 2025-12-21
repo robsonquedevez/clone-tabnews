3d70f3773c400abced63fb056a393d4f403f4772
Robson Quedevez <robson.quedevez@hotmail.com>
2025-12-21 19:17:45 -0300

    feat: add active user method  in `orchestrator.js` for using tests

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -4,6 +4,7 @@ import database from "infra/database";
 import migrator from "models/migrator";
 import user from "models/user";
 import session from "models/session";
+import activation from "models/activation";
 
 const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
 
@@ -82,6 +83,10 @@ function extractUUID(text) {
   return match ? match[0] : null;
 }
 
+async function activateUser(inactiveUser) {
+  return await activation.activateUserByUserId(inactiveUser.id);
+}
+
 const orchestrator = {
   waitForAllServices,
   cleanDatabase,
@@ -91,6 +96,7 @@ const orchestrator = {
   deleteAllEmails,
   getLastEmail,
   extractUUID,
+  activateUser,
 };
 
 export default orchestrator;
